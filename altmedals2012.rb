cc443adb19d9b3b4cf987e4342b5dfe8d1fc0b7a
Jez Cope <J.Cope@bath.ac.uk>
2012-08-03 13:04:34 +0100

    Create a Nation model.

--- altmedals2012.rb
+++ altmedals2012.rb
@@ -1,8 +1,25 @@
 require 'camping'
+require 'httpclient'
+require 'json'
 
 Camping.goes :AltMedals2012
 
 module AltMedals2012::Models
+  class Nation
+    def initialize(name, code, gold, silver, bronze)
+      @name = name
+      @code = code
+      @gold = gold
+      @silver = silver
+      @bronze = bronz
+    end
+
+    attr_accessor :name
+    attr_accessor :code
+    attr_accessor :gold
+    attr_accessor :silver
+    attr_accessor :bronze
+  end
 end
 
 module AltMedals2012::Controllers
