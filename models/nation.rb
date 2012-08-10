7ce289188c6950b5c2e44dafe2f9e754fa26a073
Jez Cope <j.cope@erambler.co.uk>
2012-08-10 17:03:15 +0100

    Do sorting in SQL for efficiency.

--- models/nation.rb
+++ models/nation.rb
@@ -9,28 +9,20 @@ class Nation < Sequel::Model
     gold + silver + bronze
   end
 
-  def weighted_total(x, y, z)
-    x*gold + y*silver + z*gold
-  end
-
-  def all_medals
-    [gold, silver, bronze]
-  end
-
   def to_s
     "Nation(#{name}: #{gold} gold, #{silver} silver, #{bronze} bronze)"
   end
 
   def self.all_by_type
-    self.all.sort_by {|nation| nation.all_medals}
+    order(:gold, :silver, :bronze)
   end
 
   def self.all_by_total
-    self.all.sort_by {|nation| nation.total}
+    order{gold + silver + bronze}
   end
 
   def self.all_by_weighted_total(x, y, z)
-    self.all.sort_by {|nation| nation.weighted_total(x,y,z)}
+    order{ self.*(x, gold) + self.*(y, silver) + self.*(z, bronze) }
   end
 
   def self.last_updated
